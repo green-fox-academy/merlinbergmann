a705f897077c77bd91854dca127c2efaee67d6b7
merlinbergmann <hello@merlin.codes>
2017-11-27 16:00:56 +0100

    excercise 10 done: DefineBasicInfo

---
+++ week-02/day1/src/DefineBasicInfo.java
@@ -0,0 +1,16 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String myName = "Merlin Bergmann";
+        // Your age as an integer
+        int myAge = 27;
+        // Your height in meters as a double
+        double myHeight = 1.8;
+        // Whether you are married or not as a boolean
+        boolean maritalStatus = false;
+        System.out.println(myName);
+        System.out.println(myHeight);
+        System.out.println(maritalStatus);
+    }
+}
