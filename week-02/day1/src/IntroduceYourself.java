80bc8380261d2996ad9be253716ee52f4418564a
merlinbergmann <hello@merlin.codes>
2017-11-27 14:51:23 +0100

    excercise 4 done: IntroduceYourself

---
+++ week-02/day1/src/IntroduceYourself.java
@@ -0,0 +1,13 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        System.out.println("Name: Merlin Bergmann");
+        //  - Your age
+        System.out.println("Age: " +27);
+        //  - Your height in meters (as a decimal fraction)
+        System.out.println("Height: " + 1.8 + " meters");
+     
+    }
+}
