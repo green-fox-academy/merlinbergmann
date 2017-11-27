6d31513a37c3b311180d3608e5adb18ba69f4309
merlinbergmann <hello@merlin.codes>
2017-11-27 15:53:28 +0100

    added inline comment for excercise 7 to clarify my solution

--- week-02/day1/src/Swap.java
+++ week-02/day1/src/Swap.java
@@ -1,9 +1,9 @@
 public class Swap {
     public static void main(String[] args) {
-        // Swap the values of the variables
+        // Swap the values of the variables a and b
         int a = 123;
         int b = 526;
-
+        //introducing new int to store the value for b before swapping.
         int swapIt;
 
         swapIt = b;
