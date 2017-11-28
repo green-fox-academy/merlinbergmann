834ab7484b3b9b1aee0c127b0d63a49d20f0b018
merlinbergmann <hello@merlin.codes>
2017-11-28 17:10:12 +0100

    excersice 1 day 2 done: Doubling

--- week-02/day2/src/Doubling.java
+++ week-02/day2/src/Doubling.java
@@ -3,6 +3,12 @@ public class Doubling {
         // - Create an integer variable named `ak` and assign the value `123` to it
         // - Create a function called `doubling` that doubles it's input parameter
         // - Print the result of `doubling(ak)`
+    int ak = 123;
+    doubling(ak);
+    }
 
+    public static void doubling(int i) {
+        i *= 2;
+        System.out.println(i);
     }
 }
