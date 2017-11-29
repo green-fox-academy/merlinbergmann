27615b4511df50c8bb5d8e17af777ff2e5a910cc
merlinbergmann <hello@merlin.codes>
2017-11-29 17:12:14 +0100

    task5 done: Reverse

---
+++ week-02/day3/src/Reverse.java
@@ -0,0 +1,16 @@
+public class Reverse {
+    public static void main(String... args){
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+        // Create a function that can reverse a String, which is passed as the parameter
+        // Use it on this reversed string to check it!
+        // Try to solve this using charAt() first, and optionally anything else after.
+
+        String fixed = "";
+        int length = reversed.length();
+        for (int i = length - 1 ; i >= 0 ; i-- ) {
+            fixed = fixed + reversed.charAt(i);
+        }
+        System.out.println(fixed);
+    }
+}
