fa6535661f86c4a8240138fb792001bc6a710492
merlinbergmann <hello@merlin.codes>
2017-11-29 09:36:26 +0100

    Excercise 9 done: Third

---
+++ week-02/day2/src/PrintArguments.java
@@ -0,0 +1,20 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `printer`
+//   which prints the input String parameters
+// - It can have any number of parameters
+
+// Examples
+// printer("first")
+// printer("first", "second")
+// printer("first", "second", "third", "fourh")
+// ...
+
+public class PrintArguments {
+
+    public static void main(String[] args) {
+        String s = "first";
+        String s1 = "second";
+    }
+}
