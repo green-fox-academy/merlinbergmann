d4a2ed14979545af2e22f091b81ab333f7b8211e
merlinbergmann <hello@merlin.codes>
2017-11-29 12:01:16 +0100

    excercise 15 done thanks to mark: Matrix

--- week-02/day2/src/Matrix.java
+++ week-02/day2/src/Matrix.java
@@ -13,6 +13,16 @@ import java.util.Arrays;
 public class Matrix {
     public static void main(String[] args) {
         int[][] ma = new int[4][4];
-        System.out.println(Arrays.deepToString(ma));
+
+        for (int row = 0; row < ma.length; row++) {
+            for (int col = 0; col < ma[row].length; col++) {
+                if (row == col){
+                    ma[row][col] = 1;
+                }
+            }
+        }
+        for (int row = 0; row < ma.length; row++) {
+            System.out.println(Arrays.toString(ma[row]));
+        }
     }
 }
