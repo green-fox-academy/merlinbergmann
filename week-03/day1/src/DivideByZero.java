63c9bd9e01d5d482fc38fe232e7fa6936ce3fe09
merlinbergmann <hello@merlin.codes>
2017-12-04 13:42:57 +0100

    week-02 day1 directory setup and gitignore file added, task1 completed: DivideByZero

---
+++ week-03/day1/src/DivideByZero.java
@@ -0,0 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+// create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// it should print "fail" if the parameter is 0
+
+public class DivideByZero {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer");
+        int divisor = scanner.nextInt();
+
+        try {
+            int result = 10 / divisor;
+            System.out.println("10 / " + divisor + " = " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("fail");
+        }
+
+    }
+}
