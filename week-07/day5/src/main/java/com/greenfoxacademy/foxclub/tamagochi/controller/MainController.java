ecbab0ff72d379ec8d36df12998e5f1fa102908c
merlinbergmann <hello@merlin.codes>
2018-01-11 18:18:07 +0100

    good to go home

--- week-07/day5/src/main/java/com/greenfoxacademy/foxclub/tamagochi/controller/MainController.java
+++ week-07/day5/src/main/java/com/greenfoxacademy/foxclub/tamagochi/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.greenfoxacademy.foxclub.tamagochi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+  @GetMapping("/")
+  public String showHome() {
+    return "index";
+  }
 }
