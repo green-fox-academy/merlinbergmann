b25bfb5990dfb51319f3eb7dcd3489807c1dfd49
merlinbergmann <hello@merlin.codes>
2018-01-23 16:16:39 +0100

    task 3 done: greeting

--- week-09/day-2/src/main/java/com/greenfoxacademy/restbackend/controllers/MainRestController.java
+++ week-09/day-2/src/main/java/com/greenfoxacademy/restbackend/controllers/MainRestController.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.restbackend.controllers;
 
 import com.greenfoxacademy.restbackend.models.Doubling;
 import com.greenfoxacademy.restbackend.models.ErrorResponse;
+import com.greenfoxacademy.restbackend.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,19 @@ public class MainRestController {
       return new ErrorResponse("Please provide an input!");
     } else {
       return new Doubling(input);
+    }
+
+  }
+
+  @GetMapping("/greeter")
+  public Object greetThis(@RequestParam(value = "name", required = false) String name
+                            , @RequestParam(value = "title", required = false) String title) {
+    if (name == null) {
+      return new ErrorResponse("Please provide a name!");
+    } else if (title == null) {
+      return new ErrorResponse("Please provide a title!");
+    } else {
+      return new Greeting(name, title);
     }
 
   }
---
+++ week-09/day-2/src/main/java/com/greenfoxacademy/restbackend/models/Greeting.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.restbackend.models;
+
+public class Greeting {
+
+  private String welcome_message;
+
+
+  public Greeting() {
+  }
+
+  public Greeting(String name, String title) {
+    this.welcome_message = "Oh, hi there " + name + ", my dear " + title + "!";
+  }
+
+  public String getWelcome_message() {
+    return welcome_message;
+  }
+
+  public void setWelcome_message(String welcome_message) {
+    this.welcome_message = welcome_message;
+  }
+}
